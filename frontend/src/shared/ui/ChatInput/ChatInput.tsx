1ee641c3f47ca4d38c33a7048387304eaf8b970e
kaden09 <ketkette7@gmail.com>
2026-02-15 12:12:26 +0400

    fix: fixed ChatInput background color

--- frontend/src/shared/ui/ChatInput/ChatInput.tsx
+++ frontend/src/shared/ui/ChatInput/ChatInput.tsx
@@ -95,7 +95,7 @@ function ChatInput({ chatId, onMessageSent, onFileSelect }: ChatInputProps) {
         />
         <textarea
           ref={textareaRef}
-          className={`bg-chat-tertiary-bg rounded-3xl text-primary-font shadow-[0_1px_2px_#00000025] placeholder:text-secondary-font w-full py-2.5 px-14 pl-23 outline-none resize-none min-h-12 max-h-75 h-12 ${styles["input-area"]}`}
+          className={`bg-chat-secondary-bg rounded-3xl text-primary-font shadow-[0_1px_2px_#00000025] placeholder:text-secondary-font w-full py-2.5 px-14 pl-23 outline-none resize-none min-h-12 max-h-75 h-12 ${styles["input-area"]}`}
           placeholder="Сообщение..."
           onInput={autoResize}
           value={value}
